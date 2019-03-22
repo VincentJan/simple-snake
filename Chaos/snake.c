20aee3ae303343a3cd14e46a0922d1e4f49dd04b
VincentJan <47446800+VincentJan@users.noreply.github.com>
2019-03-22 19:33:47 +0800

    Update snake.c

--- Chaos/snake.c
+++ Chaos/snake.c
@@ -6,21 +6,25 @@ char b[100][41];int flag=1;int number=5;int locnum=0; int  timer=100;  int score
 struct location{int sta;int x1;int y1; int flag;}   ;  struct bodys{  int statu; int x2;
 int y2;};struct bodys body[1000];struct location  loc[10000];void init();void refresh();
 void move();void play();void food();void eatfood() ;int main(){printf("PressEnter\n")  ;
-getchar();init();play();	system("cls");printf( "You die!\n""Your score is: %d\n"    , 
-score);getchar();return 0; }void   init()	  { int x ,  y  ,  j  , k;	   HANDLE hOut = 
+getchar();init();play();	system("cls");   printf( "You die!\n""Your score is: %d\n"    , 
+score);getchar(); return 0;   }void   init()	  { int x ,  y  ,  j  , k;	   HANDLE hOut = 
 GetStdHandle(STD_OUTPUT_HANDLE);CONSOLE_CURSOR_INFO cci;GetConsoleCursorInfo(hOut,&cci);
 cci.bVisible = 0;SetConsoleCursorInfo(hOut, &cci);system("mode con cols=100 lines=42") ;
 for (y = 0; y <= 40; y++){for (x = 0; x <= 99; x++){b[x][y] = ' ';if (x == 0 || x == 99
  || y == 0 || y == 40){b[x][y] = '#';}printf("%c"  , b[x][y]);}printf("\n");}for (j = 0;
   j < number; j++){body[j].statu = 1;body[j].x2 = 40 - j * 2;body[j].y2 = 20;if (j == 0)
-{b[body[j].x2][body[j].y2] = '@';}else{b[body[j].x2][body[j].y2] = '0';}}	for (k = 0 ;
+{b[body[j].x2][body[j].y2] = '@';}else{b[body[j].x2][body[j].y2] = '0';}}	for (k   = 0 ;
 k < 10000; k++)	{loc[k].flag = 0;}body[0].statu = 1;}void refresh() {int x,y; int f = 0;
 system("cls");for (y = 0; y <= 40; y++){for (x = 0; x <= 99; x++){printf("%c", b[x][y]);
 }printf("\n");}}void move(int bo){switch(body[bo].statu){case 1:if (b [body[bo] .x2 + 2]
 [body[bo].y2] != ' ' && b[body[bo].x2 + 2][body[bo].y2] != '$'){flag = 0;break;}  b[body
 [bo].x2 + 2][body[bo].y2] = b[body[bo].x2][body[bo].y2];b[body[bo].x2][body[bo].y2]=' ';
-body[bo].x2++;	body[bo].x2++;if (b[body[bo].x2 + 2][body[bo].y2] == '$') {eatfood (); }
-break;case 2:if (b[body[bo].x2][body[bo].y2 +1] != ' ' && b[body[bo].x2][body[bo].y2 +1]
+body[bo].x2++;	body[bo].x2++;if (b[body[bo].x2 + 2][body[bo].y2] == '$')  {eatfood (); }
+
+                                "Make a contract with me"
+							                        "and become a magical girl!"
+
+;break;case 2:if (b[body[bo].x2][body[bo].y2 +1] !=' ' && b[body[bo].x2][body[bo].y2 +1]
  != '$'){flag = 0;break;}b[body[bo].x2][body[bo].y2 + 1] = b[body[bo].x2][body[bo].y2];b
  [body[bo].x2][body[bo].y2] = ' ';body[bo].y2++;if(b[body[bo].x2][body[bo].y2 + 1]=='$')
 {eatfood();}break;case 3:if (b[body[bo].x2 - 2][body[bo].y2] != ' ' && b[body[bo].x2 -2]
